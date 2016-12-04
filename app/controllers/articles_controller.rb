338ca0cf5a80ad70f5f7dc3d7b5d7ba5776814e1
shsv382 <shsv382@gmail.com>
2016-12-04 16:40:57 +0300

    First blogger commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,28 @@
+class ArticlesController < ApplicationController
+
+	def index
+		@articles = Article.all
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(article_params)
+		if @article.save
+			redirect_to '/articles'
+		else
+			render 'new'
+		end
+	end
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
+	private
+	def article_params
+		params.require(:article).permit(:title, :body)
+	end
+end
